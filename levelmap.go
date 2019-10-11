2eebc052fd64cbfab8eca86ee9b83d79d3ebdeb3
Dmitriy Belov <dkhasanov@avito.ru>
2019-10-12 00:12:55 +0300

    Fixed rendering bug.

--- levelmap.go
+++ levelmap.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 // LevelMap is a 2d map of a level.
 type LevelMap struct {
 	Width   int
@@ -8,7 +10,13 @@ type LevelMap struct {
 }
 
 // GetTile returns tile of a map from the specified coord.
-func (m *LevelMap) GetTile(x int, y int) Tile {
+func (m *LevelMap) GetTile(x int, y int) (Tile, error) {
+	if x >= m.Width {
+		return Tile('X'), errors.New("X coord out of range.")
+	}
+	if y >= m.Height {
+		return Tile('X'), errors.New("Y coord out of range.")
+	}
 	idx := y*m.Width + x
-	return m.Texture[idx]
+	return m.Texture[idx], nil
 }
