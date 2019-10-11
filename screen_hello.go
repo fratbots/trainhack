9f10817eaaf0f8fec466d3cd700b3ef03a2e3a22
Qinabu <qinabu@gmail.com>
2019-10-11 15:45:56 +0300

    ~

--- screen_hello.go
+++ screen_hello.go
@@ -9,7 +9,8 @@ type HelloScreen struct {
 
 func (s *HelloScreen) Do(g *Game, end func(next Screen)) tview.Primitive {
 
-	modal := tview.NewModal().
+	var modal *tview.Modal
+	modal = tview.NewModal().
 		SetText("Hello, чувак!").
 		AddButtons([]string{"Play", "Exit"}).
 		SetDoneFunc(
@@ -19,7 +20,7 @@ func (s *HelloScreen) Do(g *Game, end func(next Screen)) tview.Primitive {
 					return
 				}
 
-				g.Final()
+				modal.SetText("You win!")
 			},
 		)
 
