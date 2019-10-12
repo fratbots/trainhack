e4a3d742a5c8668aa2fd082fd1b2bfe22fbdf1f4
Qinabu <qinabu@gmail.com>
2019-10-12 11:36:44 +0300

    fatigue

--- behavior.go
+++ behavior.go
@@ -10,6 +10,11 @@ func abs(a int) int {
 func Pursue(actor *Actor, stage *Stage, target *Actor) *Actor {
 
 	actor.Behavior = func() *Action {
+		if abs(target.Position.X-actor.Position.X) > 5 ||
+			abs(target.Position.Y-actor.Position.Y) > 5 {
+			return nil
+		}
+
 		if abs(target.Position.X-actor.Position.X) > abs(target.Position.Y-actor.Position.Y) {
 			if target.Position.X-actor.Position.X < 0 {
 				return ActionMove(stage, actor, DirectionLeft)
