cd53e7d9e413be36f2b9923ce66d1995075872a4
Dmitriy Belov <dkhasanov@avito.ru>
2019-10-12 17:25:58 +0300

    Charaster ASCII editor added.

---
+++ docs/charaster/charaster_files/themes.js
@@ -0,0 +1,163 @@
+"use strict";
+
+var themes = [];
+
+// Stereokai theme sourced from https://github.com/carlcalderon/sublime-color-schemes/blob/master/Stereokai/Stereokai.tmTheme
+themes["Stereokai"] = new Theme(
+  "Stereokai",
+  "#f8f2f2",
+  "#262626",
+  "#333333",
+  "#ffffff",
+  [
+    "#262626",
+    "#cc0000",
+    "#a6e22e",
+    "#cfcf19",
+    "#529bff",
+    "#d81dc3",
+    "#ae81ff",
+    "#d0d0d0",
+    "#3e3e3e",
+    "#fb4d53",
+    "#74ff79",
+    "#e6db74",
+    "#66d9ef",
+    "#f63ba3",
+    "#2ad2f7",
+    "#f8f8f0"
+  ]
+);
+
+// Solarized theme sourced from https://github.com/ghuntley/terminator-solarized
+themes["Solarized Dark"] = new Theme(
+  "Solarized Dark",
+  "#eee8d5",
+  "#002b36",
+  "#073642",
+  "#586e75",
+  [
+    "#073642",
+    "#dc322f",
+    "#859900",
+    "#b58900",
+    "#268bd2",
+    "#d33682",
+    "#2aa198",
+    "#eee8d5",
+    "#586e75",
+    "#cb4b16",
+    "#586e75",
+    "#657b83",
+    "#839496",
+    "#6c71c4",
+    "#93a1a1",
+    "#fdf6e3"
+  ]
+);
+themes["Solarized Light"] = new Theme(
+  "Solarized Light",
+  "#536870",
+  "#fcf4dc",
+  "#ffffff",
+  "#586e75",
+  [
+    "#073642",
+    "#dc322f",
+    "#859900",
+    "#b58900",
+    "#268bd2",
+    "#d33682",
+    "#2aa198",
+    "#eee8d5",
+    "#586e75",
+    "#cb4b16",
+    "#586e75",
+    "#657b83",
+    "#839496",
+    "#6c71c4",
+    "#93a1a1",
+    "#fdf6e3"
+  ]
+);
+
+// Ubuntu theme sourced from https://github.com/mbadolato/iTerm2-Color-Schemes
+themes["Ubuntu"] = new Theme(
+  "Ubuntu",
+  "#eeeeec",
+  "#300a24",
+  "#333333",
+  "#bbbbbb",
+  [
+    "#2e3436",
+    "#cc0000",
+    "#4e9a06",
+    "#c4a000",
+    "#3465a4",
+    "#75507b",
+    "#06989a",
+    "#d3d7cf",
+    "#555753",
+    "#ef2929",
+    "#8ae234",
+    "#fce94f",
+    "#729fcf",
+    "#ad7fa8",
+    "#34e2e2",
+    "#eeeeec"
+  ]
+);
+
+// Tango theme sourced from https://blogs.n1zyy.com/andrew/2009/02/02/tango-color-scheme-for-xfce-terminal/
+themes["Tango"] = new Theme(
+  "Tango",
+  "#323232",
+  "#ffffff",
+  "#eeeeee",
+  "#555555",
+  [
+    "#2e3436",
+    "#cc0000",
+    "#4e9a06",
+    "#c4a000",
+    "#3465a4",
+    "#75507b",
+    "#06989a",
+    "#d3d7cf",
+    "#555753",
+    "#ef2929",
+    "#8ae234",
+    "#fce94f",
+    "#729fcf",
+    "#ad7fa8",
+    "#34e2e2",
+    "#eeeeec"
+  ]
+);
+
+// Obsidian theme sourced from https://github.com/mbadolato/iTerm2-Color-Schemes
+themes["Obsidian"] = new Theme(
+  "Obsidian",
+  "#cdcdcd",
+  "#283033",
+  "#3d4447",
+  "#c0cad0",
+  [
+    "#000000",
+    "#a60001",
+    "#00bb00",
+    "#fecd22",
+    "#3a9bdb",
+    "#bb00bb",
+    "#00bbbb",
+    "#bbbbbb",
+    "#555555",
+    "#ff0003",
+    "#93c863",
+    "#fef874",
+    "#a1d7ff",
+    "#ff55ff",
+    "#55ffff",
+    "#ffffff"
+  ]
+);
