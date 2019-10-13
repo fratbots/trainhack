7707a07b676d8a7a6ae220188e51ed9adf4408a7
Dmitriy Belov <dkhasanov@avito.ru>
2019-10-13 18:43:21 +0300

    Removed debug from main.

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"log"
 	"os"
-	"time"
 )
 
 func main() {
 	os.Setenv("TERM", "xterm-256color")
 
-	themeSoundsTest()
-
-	// game := NewGame()
-	// game.Start(&HelloScreen{})
-}
-
-func themeSoundsTest() {
-	soundLibrary, err := NewSoundLibrary()
-	if err != nil {
-		log.Fatalf("Failed to init sound library: %v", err)
-	}
-	soundLibrary.SetTheme(SoundThemeAutumn)
-	time.Sleep(time.Second * 2)
-	soundLibrary.SetTheme(SoundThemePursuit)
-	time.Sleep(time.Second * 2)
+	game := NewGame()
+	game.Start(&HelloScreen{})
 }
