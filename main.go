cbe52186e49e0d00742a7bcbb03a95b64da3a845
Dmitriy Belov <dkhasanov@avito.ru>
2019-10-13 18:42:44 +0300

    Merged conflict state.

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"github.com/gdamore/tcell"
-	"github.com/rivo/tview"
 	"log"
 	"os"
 	"time"
@@ -11,10 +9,10 @@ import (
 func main() {
 	os.Setenv("TERM", "xterm-256color")
 
-	// themeSoundsTest()
-	//   lolkek()
-	game := NewGame()
-	game.Start(&HelloScreen{})
+	themeSoundsTest()
+
+	// game := NewGame()
+	// game.Start(&HelloScreen{})
 }
 
 func themeSoundsTest() {
@@ -26,19 +24,4 @@ func themeSoundsTest() {
 	time.Sleep(time.Second * 2)
 	soundLibrary.SetTheme(SoundThemePursuit)
 	time.Sleep(time.Second * 2)
-}
-
-func lolkek() {
-	app := tview.NewApplication()
-	frame := tview.NewFrame(tview.NewBox().SetBackgroundColor(tcell.ColorBlue)).
-		SetBorders(2, 2, 2, 2, 4, 4).
-		AddText("Header left", true, tview.AlignLeft, tcell.ColorWhite).
-		AddText("Header middle", true, tview.AlignCenter, tcell.ColorWhite).
-		AddText("Header right", true, tview.AlignRight, tcell.ColorWhite).
-		AddText("Header second middle", true, tview.AlignCenter, tcell.ColorRed).
-		AddText("Footer middle", false, tview.AlignCenter, tcell.ColorGreen).
-		AddText("Footer second middle", false, tview.AlignCenter, tcell.ColorGreen)
-	if err := app.SetRoot(frame, true).SetFocus(frame).Run(); err != nil {
-		panic(err)
-	}
 }
