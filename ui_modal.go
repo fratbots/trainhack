bc2122642da618eba67ea5dba80f064a183333d6
Qinabu <qinabu@gmail.com>
2019-10-25 22:39:29 +0300

    fix modal

--- ui_modal.go
+++ ui_modal.go
@@ -9,16 +9,18 @@ func NewUIModal(text string, buttons ...interface{}) tview.Primitive {
 	p.SetText(text)
 
 	var functions []func()
+	var texts []string
 
 	for i := 0; i < len(buttons); i += 2 {
 		if s, ok := buttons[i].(string); ok {
-			p.AddButtons([]string{s})
+			texts = append(texts, s)
 		}
-		if p, ok := buttons[i+1].(func()); ok {
-			functions = append(functions, p)
+		if f, ok := buttons[i+1].(func()); ok {
+			functions = append(functions, f)
 		}
 	}
 
+	p.AddButtons(texts)
 	p.SetDoneFunc(func(i int, l string) {
 		if i < len(functions) {
 			functions[i]()
