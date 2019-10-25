2ded440fe5fcb680a20090b6f6572be1e141e7b2
Dmitriy Belov <dkhasanov@avito.ru>
2019-10-25 22:38:21 +0300

    Fixed bug with exit button.

--- ui_modal.go
+++ ui_modal.go
@@ -10,14 +10,17 @@ func NewUIModal(text string, buttons ...interface{}) tview.Primitive {
 
 	var functions []func()
 
+	buttonsList := make([]string, len(buttons)/2)
 	for i := 0; i < len(buttons); i += 2 {
 		if s, ok := buttons[i].(string); ok {
-			p.AddButtons([]string{s})
+			buttonsList[(i+1)/2] = s
 		}
 		if p, ok := buttons[i+1].(func()); ok {
 			functions = append(functions, p)
 		}
 	}
+
+	p.AddButtons(buttonsList)
 
 	p.SetDoneFunc(func(i int, l string) {
 		if i < len(functions) {
