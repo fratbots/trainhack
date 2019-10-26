1fe83518d655b0a9b8ad92e464f6c10a86ea0cda
Dmitriy Belov <dkhasanov@avito.ru>
2019-10-26 12:16:56 +0300

    Animated aura effect created.

--- effect_aura.go
+++ effect_aura.go
@@ -1,11 +1,13 @@
 package main
 
+// EffectAura is an Aura effect around actor.
 type EffectAura struct {
 	behavior EffectBehavior
 	age      int
 	target   *Actor
 }
 
+// NewEffectAura creates Aura effect.
 func NewEffectAura(behavior EffectBehavior, age int, target *Actor) *EffectAura {
 	return &EffectAura{
 		behavior: behavior,
@@ -14,14 +16,17 @@ func NewEffectAura(behavior EffectBehavior, age int, target *Actor) *EffectAura 
 	}
 }
 
+// GetBehavior returns Aura effect behavior.
 func (e *EffectAura) GetBehavior() EffectBehavior {
 	return e.behavior
 }
 
+// GetTarget returns actor that Aura effect assigned to.
 func (e *EffectAura) GetTarget() *Actor {
 	return e.target
 }
 
+// Equals checks whether other effect is also Aura effect that is assigned to the same actor.
 func (e *EffectAura) Equals(other Effect) bool {
 	if otherEffectAura, ok := other.(*EffectAura); ok {
 		if e.GetTarget() == otherEffectAura.GetTarget() {
@@ -31,6 +36,7 @@ func (e *EffectAura) Equals(other Effect) bool {
 	return false
 }
 
+// Update moves forward effect animation progress and returns false if animation has ended.
 func (e *EffectAura) Update() bool {
 	e.age = e.age - 1
 	if e.age <= 0 {
@@ -40,22 +46,39 @@ func (e *EffectAura) Update() bool {
 }
 
 func (e *EffectAura) Render() []EffectTile {
+	var runeLeft rune
+	var runeRight rune
+	var runeTop rune
+	var runeBottom rune
+
+	if e.age <= 6 {
+		runeLeft = '+'
+		runeRight = '+'
+		runeTop = '+'
+		runeBottom = '+'
+	} else {
+		runeLeft = '*'
+		runeRight = '*'
+		runeTop = '*'
+		runeBottom = '*'
+	}
+
 	return []EffectTile{
 		EffectTile{
 			Position: Position{e.target.Position.X - 1, e.target.Position.Y},
-			Rune:     '-',
+			Rune:     runeLeft,
 		},
 		EffectTile{
 			Position: Position{e.target.Position.X + 1, e.target.Position.Y},
-			Rune:     '-',
+			Rune:     runeRight,
 		},
 		EffectTile{
 			Position: Position{e.target.Position.X, e.target.Position.Y - 1},
-			Rune:     '|',
+			Rune:     runeTop,
 		},
 		EffectTile{
 			Position: Position{e.target.Position.X, e.target.Position.Y + 1},
-			Rune:     '|',
+			Rune:     runeBottom,
 		},
 	}
 }
