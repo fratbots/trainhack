fc945b885008b017ee36a2fe4fcc8357eccf19aa
Dmitriy Belov <dkhasanov@avito.ru>
2019-10-26 12:11:43 +0300

    Aura effect moved to distinct file.

---
+++ effect_aura.go
@@ -0,0 +1,61 @@
+package main
+
+type EffectAura struct {
+	behavior EffectBehavior
+	age      int
+	target   *Actor
+}
+
+func NewEffectAura(behavior EffectBehavior, age int, target *Actor) *EffectAura {
+	return &EffectAura{
+		behavior: behavior,
+		age:      age,
+		target:   target,
+	}
+}
+
+func (e *EffectAura) GetBehavior() EffectBehavior {
+	return e.behavior
+}
+
+func (e *EffectAura) GetTarget() *Actor {
+	return e.target
+}
+
+func (e *EffectAura) Equals(other Effect) bool {
+	if otherEffectAura, ok := other.(*EffectAura); ok {
+		if e.GetTarget() == otherEffectAura.GetTarget() {
+			return true
+		}
+	}
+	return false
+}
+
+func (e *EffectAura) Update() bool {
+	e.age = e.age - 1
+	if e.age <= 0 {
+		return false
+	}
+	return true
+}
+
+func (e *EffectAura) Render() []EffectTile {
+	return []EffectTile{
+		EffectTile{
+			Position: Position{e.target.Position.X - 1, e.target.Position.Y},
+			Rune:     '-',
+		},
+		EffectTile{
+			Position: Position{e.target.Position.X + 1, e.target.Position.Y},
+			Rune:     '-',
+		},
+		EffectTile{
+			Position: Position{e.target.Position.X, e.target.Position.Y - 1},
+			Rune:     '|',
+		},
+		EffectTile{
+			Position: Position{e.target.Position.X, e.target.Position.Y + 1},
+			Rune:     '|',
+		},
+	}
+}
